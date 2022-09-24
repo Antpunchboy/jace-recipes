e5bc8bb7a04171ec2993f5a10715b67d5aea5553
Antpunchboy <jaceantman@yahoo.com>
2022-09-24 16:41:49 +0800

    add different results for empty "sign up" button prompts and cancelled prompts

--- javascript.js
+++ javascript.js
@@ -1,5 +1,10 @@
 function sign_up() {
     email = prompt(`Type in your email for a free 1 month subscription
 (don't actually put in email, this is just a sample)`);
-    alert(`We will be sending you your subscription details shortly to ${email}`);
+    if (email !== "" && email !== null) {
+        alert(`We will be sending you your subscription details shortly to ${email}`);
+    }
+    else if (email === "") {
+        alert("No email entered");
+    };
 };
