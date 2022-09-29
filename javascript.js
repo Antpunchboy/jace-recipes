bc0ddc2d922a22ec8d1fcc25278f8c6ce1b20157
Antpunchboy <jaceantman@yahoo.com>
2022-09-29 13:15:08 +0800

    make "Sign up!" button reject spacing prompt inputs and state 'No email entered"

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 function sign_up() {
     email = prompt(`Type in your email for a free 1 month subscription
 (don't actually put in email, this is just a sample)`);
-    if (email !== "" && email !== null) {
+    if (email.trim() !== "" && email !== null) {
         alert(`We will be sending you your subscription details shortly to ${email}`);
     }
-    else if (email === "") {
+    else if (email.trim() === "") {
         alert("No email entered");
     };
     location.reload();
